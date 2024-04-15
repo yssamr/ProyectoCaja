fde96f7398b6408c8e0f631871c7577f63ebd83a
Alejandro Beltre <1115242@est.intec.edu.do>
2024-04-14 21:14:03 -0400

    Añadi la captura de data del registro, cambie txtSucursal por comboBoxSucursal, elimine boton cancelar

--- RegistrarCajero.cs
+++ RegistrarCajero.cs
@@ -12,6 +12,52 @@ namespace AppCaja
 {
     public partial class RegistrarCajero : Form
     {
+        string nombres, apellidos, cedula, telefono, direccion, usuario, sucursal, contraseña, opcionRadioButton, rolUsuario;
+        bool politicas = false;
+
+        private void cbPolitica_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbPolitica.Checked)
+            {
+                politicas = true;
+            }
+            else
+            {
+                politicas = false;
+            }
+        }
+
+        private void rbCajero_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbCajero.Checked)
+            {
+                opcionRadioButton = "Cajero";
+            }
+        }
+
+        private void rbAdmin_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rbAdmin.Checked)
+            {
+                opcionRadioButton = "Administrador";
+            }
+        }
+
+        private void btnRegistrarse_Click(object sender, EventArgs e)
+        {
+            nombres = txtNombres.Text;
+            apellidos = txtApellidos.Text;
+            cedula = txtCedula.Text;
+            telefono = txtTelefono.Text;
+            direccion = txtDireccion.Text;
+            usuario = txtUsuario.Text;
+            sucursal = comboBoxSucursal.SelectedIndex.ToString();
+            contraseña = txtContraseña.Text;
+            rolUsuario = opcionRadioButton;
+
+            MessageBox.Show("¡Sus datos han sido guardados exitosamente!", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
         public RegistrarCajero()
         {
             InitializeComponent();
@@ -28,6 +74,11 @@ namespace AppCaja
         }
 
         private void lblPoliticas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNombres_TextChanged(object sender, EventArgs e)
         {
 
         }
