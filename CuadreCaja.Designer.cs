e6279682edaca8b1033739887ff581f40241f92c
Priscilla Castro <priscilla05marie@gmail.com>
2024-04-15 15:53:30 -0400

    Cuadre de Caja

--- CuadreCaja.Designer.cs
+++ CuadreCaja.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CuadreCaja));
+            this.SuspendLayout();
+            // 
+            // CuadreCaja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(944, 532);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CuadreCaja";
             this.Text = "CuadreCaja";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
